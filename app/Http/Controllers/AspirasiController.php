7f99483d50c1a13a67453ee4e620abe75ee0d8bc
fakhryfernanda <mff023@gmail.com>
2022-12-24 09:39:30 +0700

    + fungsi store

--- app/Http/Controllers/AspirasiController.php
+++ app/Http/Controllers/AspirasiController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class AspirationController extends Controller
 {
-    //
+    function store(Request $request)
+    {
+        $payload = $request->all();
+        
+        $columns = ["aspirator", "nik", "story", "photo", "is_read"];
+        foreach($columns as $col) {
+            if (!isset($payload[$col])) {
+                $message = "{$col} tidak boleh kosong";
+                return response()->json([
+                    "status" => false,
+                    "message" => $message,
+                    "data" => null
+                ]);
+            }
+        }
+
+        $payload['photo'] = $request->file("photo")->store("images", "public");
+        
+        $author = Aspirasi::create($payload);
+        return response()->json([
+            "status" => true,
+            "message" => "Aspirasi berhasil ditambahkan",
+            "data" => $author->makeHidden([
+                "id",
+                "created_at",
+                "updated_at"
+            ])
+        ]);
+    }
 }
