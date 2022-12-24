a8540ae3cd829172d900314c5e99b547aa045dc9
fakhryfernanda <mff023@gmail.com>
2022-12-24 09:27:28 +0700

    Import

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::post("admin/add", [AdminController::class, "store"]);
