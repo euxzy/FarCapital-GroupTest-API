31072e2e1f343f4a73ef8ae16e15a623c94e8b27
euxzy <rijal.m12l@gmail.com>
2022-12-24 09:42:34 +0700

    [ + ] create route aspiration add

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AspirationController;
 
 Route::post("admin/add", [AdminController::class, "store"]);
+
+
+Route::prefix('/aspiration')
+  ->controller(AspirationController::class)
+  ->group(function () {
+    Route::post('/add', 'store');
+  });
