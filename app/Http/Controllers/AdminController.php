2ebdd1c04d65f019b4cf50c9193157ee39c18c34
fakhryfernanda <mff023@gmail.com>
2022-12-24 09:23:51 +0700

    Membuat route api dan fungsi store

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    function store(Request $request)
+    {
+        $payload = $request->all();
+        
+        $columns = ["email", "password"];
+        foreach($columns as $col) {
+            if (!isset($payload[$col])) {
+                $message = "{$col} tidak boleh kosong";
+                return response()->json([
+                    "status" => false,
+                    "message" => $message,
+                    "data" => null
+                ]);
+            }
+        }
+
+        $admin = Admin::create($payload);
+        return response()->json([
+            "status" => true,
+            "message" => "Admin berhasil ditambahkan",
+            "data" => $admin->makeHidden([
+                "id",
+                "created_at",
+                "updated_at"
+            ])
+        ]);
+    }
 }
